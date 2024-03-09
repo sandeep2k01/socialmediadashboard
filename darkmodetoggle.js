2f33c05bddb906e4c229bfe8b18ae481fc4a89b9
Sandeep Damera <151929853+sandeep2k01@users.noreply.github.com>
2024-03-09 21:40:44 +0530

    Add files via upload

---
+++ darkmodetoggle.js
@@ -0,0 +1,19 @@
+  /* Dark Mode */
+
+ // Select the button
+const btn = document.getElementById("toggle");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "style.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "dark-theme.css";
+  // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "style.css";
+  }
+});
